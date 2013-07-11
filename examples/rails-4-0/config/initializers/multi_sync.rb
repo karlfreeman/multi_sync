e190e433a5c8d333e1f054e8147ff353a0411a91
Karl Freeman <karlfreeman@gmail.com>
2013-07-11 10:13:05 +0100

    tidy up

--- examples/rails-4-0/config/initializers/multi_sync.rb
+++ examples/rails-4-0/config/initializers/multi_sync.rb
@@ -1,11 +1,10 @@
-MultiSync.verbose = true
-
 MultiSync.configuration do |config|
-  config.verbose = true  # turn on verbose logging (defaults to false)
+  # config.verbose = false  # turn on verbose logging (defaults to false)
+  # config.force = false  # force syncing of outdated_files (defaults to false)
   # config.sync_outdated_files = true # when an outdated file is found whether to replace it (defaults to true)
   # config.delete_abandoned_files = true # when an abondoned file is found whether to remove it (defaults to true)
   # config.upload_missing_files = true # when a missing file is found whether to upload it (defaults to true)
-  # config.target_pool_size = 16 # how many threads you would like to open for each target (defaults to the amount of CPU core's your machine has)
+  # config.target_pool_size = 8 # how many threads you would like to open for each target (defaults to the amount of CPU core's your machine has)
   # config.max_sync_attempts = 1 # how many times a file should be retried if there was an error during sync (defaults to 3)
 end
 
