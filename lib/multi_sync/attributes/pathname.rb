3e3d3cc7d41fec61a10a625ef81c85a9e1aa96ab
Karl Freeman <karlfreeman@gmail.com>
2014-03-14 13:04:46 +0000

    more coerecable

--- lib/multi_sync/attributes/pathname.rb
+++ lib/multi_sync/attributes/pathname.rb
@@ -5,7 +5,20 @@ module MultiSync
   module Attributes
     class Pathname < ::Virtus::Attribute
       def coerce(value)
-        ::Pathname.new(value)
+        return ::Pathname.new(value) unless value.nil?
+        value
+      end
+
+      def value_coerced?(value)
+        value.is_a?(::Pathname)
+      end
+
+      def coercion_method
+        :to_s
+      end
+
+      def primitive
+        ::Pathname
       end
     end
   end
